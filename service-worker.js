d5c3da839db5f59fac1ef6699dbd218e9c08a055
Bmac5252 <121689839+Bmac5252@users.noreply.github.com>
2025-02-09 15:49:28 -0500

    initial upload of intl property price converter

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'real-estate-converter-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// Install service worker and cache resources
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch resources from cache or network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
