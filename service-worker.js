8ea10d055e8de4b3506fffd938095cc045be8e7f
Bmac5252 <121689839+Bmac5252@users.noreply.github.com>
2025-02-11 07:44:24 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,36 @@
-const CACHE_NAME = 'real-estate-converter-v2';
+const CACHE_NAME = 'international-property-converter-v1';
 const urlsToCache = [
   '/',
-  '/converter.html',
+  '/index.html',
   '/manifest.json',
-  '/sw.js',
+  '/converter.html',
+  '/icon-192x192.png',
+  '/icon-512x512.png',
   'https://v6.exchangerate-api.com/v6/bd5a904113f08d2c2a94bc51/latest/USD'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
-        // Return cached version or fetch from network
+      .then((response) => {
+        // Return cached response or fetch from network
         return response || fetch(event.request);
       })
   );
 });
 
-// Clean up old caches
-self.addEventListener('activate', event => {
+self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
+    caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map(cacheName => {
+        cacheNames.map((cacheName) => {
           if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
